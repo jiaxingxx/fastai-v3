d33cd63bb45ad6efc3a53a08b1d0754450468fa1
jiaxing <47101642+jiaxingxx@users.noreply.github.com>
2019-10-17 18:17:32 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.googleapis.com/drive/v3/files/1_0Hd-0j0qIyJ8uaLyhnzKatBSat8LTeE?alt=media&key=AIzaSyAG_VPgGIMKbT4D5pUZ1_uVmsERucTiwMg'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1R6LHWQ_PdNNGMKwXZTkXRJupD6PzsGWS?alt=media&key=AIzaSyAG_VPgGIMKbT4D5pUZ1_uVmsERucTiwMg'
 export_file_name = 'export.pkl'
 
 classes = ['hg', 'ljx', 'wyz']
